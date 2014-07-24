41bd4d87dbec802ca4b5d040950d4b6514d83669
ezrasmart <ezrasarajinsky@Ezra-Sarajinskys-MacBook-Pro.local>
2014-07-24 20:25:37 +1000

    initial commit

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Ezra Sarajinsky</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- meta tags-->
+	<meta name="description" content="Digital producer" />
+	<meta property="og:title" content="Ezra Sarajinsky" />
+	<meta property="og:description" content="Digital producer" />
+	<meta property="og:site_name" content="Ezra Sarajinsky"/>
+	<meta property="og:type" content="website"/>
+	<link rel="canonical" href="http://www.ezrasarajinsky.com" />
+	<link rel="image_src" href="http://www.ezrasarajinsky.com/appleiphone.png" />
+	<meta property="og:image" content="http://www.ezrasarajinsky.com/images/ezra-sarajinsky.png" />
+	<meta property="og:url" content="http://www.ezrasarajinsky.com" />
+
+	<!-- Cascading Style Sheets -->
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.css" rel="stylesheet" />
+
+	<!-- Javascript -->
+	<script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
+	<script src="js/slide.js" type="text/javascript"></script>
+
+</head>
+<body>
+	<div id="toppanel">
+		<div id="panel">
+			<div class="content clearfix">
+				<div id="sliderMenu">
+		<h1>Ezra Sarajinsky</h1>
+<div class="portrait"></div>
+		<p>I'm a digital producer with a passion for online strategy, user experience, design, and the opportunities that lie at the intersection of people and technology.</p>
+		<p>I specialise in being a jack of all digital trades. From client relationships to the intricacies of PHP and the gamut that lies in between and on the sides. That's my strength as a producer.</p>
+		<p>I'm a Project Manager with a proven track record for delivering leading digital media projects - from EDM campaigns, mobile sites, and social media integrations to microsites and full scale website builds.</p>	
+		<div class="contactPath">
+			<a href="http://au.linkedin.com/in/ezrasarajinsky" target="blank"><i class="fa fa-linkedin-square"></i></a>
+			<a href="http://www.twitter.com" target="blank"><i class="fa fa-twitter"></i></a>
+			<a href="mailto:ezra@ezrasarajinsky.com">ezra@ezrasarajinsky.com</a>
+		</div> 
+				</div>
+			</div>
+		</div> 
+
+		<div class="tab">
+			<ul class="login">
+				<li id="toggle">
+					<a id="open" class="open" href="#">About This Guy</a>
+					<a id="close" style="display: none;" class="close" href="#"></a>			
+				</li>
+			</ul> 
+		</div> 
+
+	</div> 
+	<header>
+		<a href="/" class="logo">Ezra Sarajinsky</a>
+	</header>
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php include 'header.php'; ?>
+
+    <div id="container">
+		<div id="content">
+
+			
+			
+<ul class="img-list">
+ 
+
+
+  <li class="portfolio1 port1">
+    <a href="http://www.laurentrompp.com" target="blank">
+      <span class="text-content port1"><span>Lauren Trompp</span></span>
+    </a>
+  </li>
+
+
+   <li class="portfolio2 port2">
+    <a href="http://www.insuranceline.com.au" target="blank">
+      <span class="text-content port2"><span>InsuranceLine</span></span>
+    </a>
+  </li>
+
+
+
+
+   <li class="portfolio3 port3">
+    <a href="http://www.eftposaustralia.com.au" target="blank">
+      <span class="text-content port3"><span>eftpos</span></span>
+    </a>
+  </li>
+
+
+  <li class="portfolio5 port5">
+    <a href="http://www.theasthmatix.com" target="blank">
+      <span class="text-content port5"><span>The Asthmatix</span></span>
+    </a>
+  </li>
+
+<li class="portfolio6 port6">
+    <a href="http://au.goodman.com/betterbusiness/" target="blank">
+      <span class="text-content port6"><span>Goodman Better Business</span></span>
+    </a>
+  </li>
+
+  <li class="portfolio7 port7">
+    <a href="http://www.jcdecaux.com.au/" target="blank">
+      <span class="text-content port7"><span>JCDecaux</span></span>
+    </a>
+  </li>
+
+  <li class="portfolio8 port8">
+    <a href="http://www.pocketlegal.me" target="blank">
+      <span class="text-content port8"><span>PocketLegal</span></span>
+    </a>
+  </li>
+
+  <li class="portfolio9 port9">
+    <a href="http://www.jackfeldstein.com/" target="blank">
+      <span class="text-content port9"><span>Jack Feldstein</span></span>
+    </a>
+  </li>
+  
+
+
+   <li class="portfolio4 port4">
+    <a href="http://seabournemusic.com" target="blank">
+      <span class="text-content port4"><span>Seabourne</span></span>
+    </a>
+  </li>
+
+
+
+
+  <li class="portfolio10 port10">
+    <a href="http://seabournemusic.com" target="blank">
+      <span class="text-content port10"><span>Circa</span></span>
+    </a>
+  </li>
+  <li class="portfolio11 port11">
+    <a href="http://apca.com.au" target="blank">
+      <span class="text-content port11"><span>APCA</span></span>
+    </a>
+  </li>
+  <li class="portfolio12 port12">
+    <a href="http://panasonicair.com.au" target="blank">
+      <span class="text-content port12"><span>Panasonic</span></span>
+    </a>
+  </li>
+</ul>
+
+		</div>
+	</div>
+</body>
+</html>
